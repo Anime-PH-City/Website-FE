a5df54e85c28cfcb94534744454fe58e0b89a921
Psami-wondah <okechukwusamuel16@gmail.com>
2022-08-22 17:03:39 +0100

    first commit

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { loginStyleCss } from "../styles/pages/login";
+
+const Login = () => {
+  return <div css={loginStyleCss}>Login</div>;
+};
+
+export default Login;
---
+++ styles/pages/home.tsx
@@ -0,0 +1,10 @@
+import { css } from "styled-components/macro";
+
+export const homeStylesCss = css`
+  font-weight: bold;
+  color: red;
+  .sam {
+    font-size: 68px;
+    color: yellow;
+  }
+`;
